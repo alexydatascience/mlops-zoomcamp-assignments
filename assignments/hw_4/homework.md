172861639bb1a917e1f73a25c8d28618e8aab021
alex <alexydatascience@gmail.com>
2022-06-24 22:57:56 +0300

    add hw_4

---
+++ assignments/hw_4/homework.md
@@ -0,0 +1,19 @@
+# Q1. Notebook
+ans: 16.19
+# Q2. Preparing the output
+ans: 19M
+# Q3. Creating the scoring script
+ans: there is no specific command to turn the notebook into a script. We just take essential code, refactor it into functions, and save it into .py file as a script.
+# Q4. Virtual environment
+ans: 08ef968f6b72033c16c479c966bf37ccd49b06ea91b765e1cc27afefe723920b
+# Q5. Parametrize the script
+```
+cd assignments/hw_4
+python scoring.py --year=2021 --month=3
+```
+ans: 16.29
+# Q6. Docker contaner
+```
+docker build -t hw_4 .
+```
+ans: 9.96
---
+++ assignments/hw_4/scoring.py
@@ -0,0 +1,50 @@
+import argparse
+import pickle
+import pandas as pd
+
+
+CATEGORICAL = ['PUlocationID', 'DOlocationID']
+
+
+def read_data(filename):
+    df = pd.read_parquet(filename)
+
+    df['duration'] = df.dropOff_datetime - df.pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[CATEGORICAL] = df[CATEGORICAL].fillna(-1).astype('int').astype('str')
+    
+    return df
+
+
+def run(year, month):
+    with open('model.bin', 'rb') as f_in:
+        dv, lr = pickle.load(f_in)
+
+    df = read_data(f'../../data/fhv_tripdata_{year:04d}-{month:02d}.parquet')
+    dicts = df[CATEGORICAL].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+    print(y_pred.mean())
+
+    return y_pred
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--year",
+        default=2021,
+        type=int
+    )
+    parser.add_argument(
+        "--month",
+        default=3,
+        type=int
+    )
+    args = parser.parse_args()
+
+    run(args.year, args.month)
